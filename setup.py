36b05713384db367e9a8f60f24d71f47b5e2b6e1
Ethan.Zhao <40779736+ethanzhao6@users.noreply.github.com>
2020-02-04 23:53:49 +1100

    add manifest

--- setup.py
+++ setup.py
@@ -4,6 +4,13 @@ setup(name='au_address_parser',
       version='1.0.0',
       description='A module parses common forms of australian addresses.',
       author='Ethan Zhao',
+      url='https://github.com/ethanzhao6/au-addr-parser',
       author_email='gigi17901@gmail.com',
       packages=['au_address_parser'],
-      install_requires=[])
+      install_requires=[],
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent'
+      ])
